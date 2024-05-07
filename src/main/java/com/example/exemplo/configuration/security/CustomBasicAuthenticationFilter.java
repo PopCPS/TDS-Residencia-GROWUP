de0e52b27e3520740ad7b21172c5e9d74cc0ca01
dmms <danielsena04@gmail.com>
2024-05-06 22:11:00 -0300

    removendo variáveis sem utilização e importando rest template

--- src/main/java/com/example/exemplo/configuration/security/CustomBasicAuthenticationFilter.java
+++ src/main/java/com/example/exemplo/configuration/security/CustomBasicAuthenticationFilter.java
@@ -4,24 +4,34 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import java.io.IOException;
 import java.util.Base64;
 
-//  OncePerRequestFilter classe de filtro do Spring Framework
-@Service
+import static com.example.exemplo.configuration.strateegia.StrateegiaRestTemplateConfiguration.STRATEEGIA_REST_TEMPLATE_BEAN;
+
+@Component
 public class CustomBasicAuthenticationFilter extends OncePerRequestFilter {
 
     private static final int BASIC_LENGTH = 6;
-    private static final String EXEMPLO_USERNAME = "alice";
-    private static final String EXEMPLO_PASSWORD = "123";
+
+    private final RestTemplate restTemplate;
 
     String resultado;
+
+    @Autowired
+    public CustomBasicAuthenticationFilter(@Qualifier(STRATEEGIA_REST_TEMPLATE_BEAN) RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
