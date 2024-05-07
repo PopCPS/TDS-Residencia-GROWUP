2fc95b1d77fee5b3807cf106cef413f2169651d8
dmms <danielsena04@gmail.com>
2024-05-07 00:28:51 -0300

    definindo StrateegiaAuthenticationProvider

--- src/main/java/com/example/exemplo/configuration/security/WebSecurityConfig.java
+++ src/main/java/com/example/exemplo/configuration/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.exemplo.configuration.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,10 +16,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class WebSecurityConfig {
 
+    private final ObjectMapper objectMapper;
     private final StrateegiaAuthenticationProvider strateegiaAuthenticationProvider;
 
-    public WebSecurityConfig(StrateegiaAuthenticationProvider strateegiaAuthenticationProvider) {
-        this.strateegiaAuthenticationProvider = strateegiaAuthenticationProvider;
+    public WebSecurityConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        this.strateegiaAuthenticationProvider = new StrateegiaAuthenticationProvider(objectMapper);
     }
 
     @Bean
