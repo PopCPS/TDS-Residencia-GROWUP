9f60da09646847408daf8f63998115bcb36b8604
dmms <danielsena04@gmail.com>
2024-05-24 08:45:44 -0300

    criando classe para mapear a resposta do json

---
+++ src/main/java/com/example/exemplo/controller/dto/PaginatedResponse.java
@@ -0,0 +1,37 @@
+package com.example.exemplo.controller.dto;
+
+import java.util.List;
+
+public class PaginatedResponse {
+
+    private List<AccessJourneyDTO> content;
+    private long totalElements;
+
+    public PaginatedResponse() {
+    }
+
+    public PaginatedResponse(long totalElements, List<AccessJourneyDTO> content) {
+        this.totalElements = totalElements;
+        this.content = content;
+    }
+
+    public PaginatedResponse(List<AccessJourneyDTO> content) {
+        this.content = content;
+    }
+
+    public List<AccessJourneyDTO> getContent() {
+        return content;
+    }
+
+    public void setContent(List<AccessJourneyDTO> content) {
+        this.content = content;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+}
